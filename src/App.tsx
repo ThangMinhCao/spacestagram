2012c5c9b0b144f66612870638f5074b734e772e
ThangMinhCao <thangminhcao@gmail.com>
2022-01-05 22:04:59 -0500

    Add logo and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import shopifyLogo from "./assets/shopify-logo.png";
 
 function App() {
   return (
-    <div className="App">
-      Spacestagram
+    <div>
+      <div className="header">
+        <img className="logo" src={shopifyLogo} alt="Shopify Logo" />
+        Spacestagram
+      </div>
+      <div className="main-container">
+        <div className="welcome">
+          Welcome to Spacestagram - The most wonderful photo library of NASA.
+        </div>
+        <div className="photo-list">
+          Photo list
+        </div>
+      </div>
     </div>
   );
 }
